8d55cf04a005c4e54dd170f9b2fe9e3764bef256
Nazar Yavnyy <nazar.tools@pm.me>
2022-10-18 17:01:40 +0300

    feat(toggle-case-cypher): support 'includes' option

--- src/features/cypher/toggleCase.ts
+++ src/features/cypher/toggleCase.ts
@@ -1,6 +1,15 @@
+type ToggleCaseCypherOptions = {
+  include?: string;
+};
+
 export const toggleCaseCypher =
-  () =>
+  ({ include }: ToggleCaseCypherOptions = {}) =>
   (input: string): string =>
     Array.from(input)
-      .map((c) => (c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase()))
+      .map((c) => {
+        if (include && !include.toLowerCase().includes(c.toLocaleLowerCase())) {
+          return c;
+        }
+        return c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+      })
       .join('');
--- tests/toggleCaseCypher.test.ts
+++ tests/toggleCaseCypher.test.ts
@@ -16,6 +16,18 @@ it('input: has non-alphabetic chars', () => {
   expect(toggleCaseCypher()('TeS2t5. ,')).toBe('tEs2T5. ,');
 });
 
+it('input: mixed case, include: one char', () => {
+  expect(toggleCaseCypher({ include: 't' })('TeSt')).toBe('teST');
+});
+
+it('input: mixed case, include: two chars', () => {
+  expect(toggleCaseCypher({ include: 'te' })('TeSt')).toBe('tEST');
+});
+
+it('input: mixed case, include: two chars mixed case', () => {
+  expect(toggleCaseCypher({ include: 'tS' })('TeStus')).toBe('tesTuS');
+});
+
 it('input: empty', () => {
   expect(toggleCaseCypher()('')).toBe('');
 });
