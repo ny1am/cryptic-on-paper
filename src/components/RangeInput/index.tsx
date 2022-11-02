73cd5e4b11bfd285189c152da36d2db39fb51295
Nazar Yavnyy <nazar.tools@pm.me>
2022-11-02 15:43:48 +0200

    feat: range input component

---
+++ src/components/RangeInput/index.tsx
@@ -0,0 +1,27 @@
+import './range.css';
+
+import { forwardRef, HTMLAttributes } from 'react';
+
+type RangeInputProps = HTMLAttributes<HTMLInputElement> & {
+  min: number;
+  max: number;
+};
+
+export const RangeInput = forwardRef<HTMLInputElement, RangeInputProps>(
+  function RangeInputNoRef(props, ref) {
+    const { min, max } = props;
+    const marks = new Array(max - min + 1).fill(0).map((_, i) => `${min + i}`);
+    return (
+      <div className="mt-1">
+        <input ref={ref} type="range" step="1" {...props} />
+        <div className="relative text-xs text-gray-500 flex justify-between" aria-hidden>
+          {marks.map((v) => (
+            <span key={v} className="w-3 text-center">
+              {v}
+            </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+);
