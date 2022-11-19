a45352c6b1825ede4de9009b261d04737eb1c03b
Nazar Yavnyy <nazar.tools@pm.me>
2022-11-19 21:23:19 +0200

    refactor: optimize encrypt performance
    add memoization / deferred value hooks to optimize rendering performance

---
+++ src/pages/encrypt/EncryptForm/EncryptedResult.tsx
@@ -0,0 +1,57 @@
+import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline';
+import copyToClipboard from 'copy-to-clipboard';
+import { useCallback, useDeferredValue, useMemo } from 'react';
+
+import { IconButton } from '@/components/IconButton';
+import { success } from '@/components/Toast';
+import { Cipher, encrypt } from '@/features/cipher';
+
+import { CipherMeta, ciphersRegister } from '../config';
+import { useCiphersPipeStore } from '../store';
+
+type FactoryType = CipherMeta['options'] extends infer R ? (opts: R) => Cipher : never;
+
+interface Props {
+  text: string;
+}
+
+export function EncryptedResult({ text }: Props) {
+  const selectedCiphers = useCiphersPipeStore((s) => s.ciphers);
+  const deferredText = useDeferredValue(text);
+
+  const pipe = useMemo(
+    () =>
+      selectedCiphers.map(({ meta }) => {
+        const factory = ciphersRegister[meta.key] as FactoryType;
+        return factory(meta.options);
+      }),
+    [selectedCiphers]
+  );
+
+  const cipherText = useMemo(() => encrypt(pipe, deferredText), [pipe, deferredText]);
+
+  const copy = useCallback(() => {
+    if (cipherText) {
+      copyToClipboard(cipherText);
+      success('Copied to clipboard!', { id: 'clipboard' });
+    }
+  }, [cipherText]);
+
+  return (
+    <div className="mt-2 grow flex flex-col items-end lg:mt-6">
+      <div className="mb-1 w-full flex justify-between items-end">
+        <span className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+          Encrypted message
+        </span>
+        <IconButton
+          title="Copy to clipboard"
+          icon={<ClipboardDocumentListIcon className="h-5" />}
+          onClick={copy}
+        />
+      </div>
+      <pre className="min-h-[8rem] w-full grow text-sm break-all whitespace-pre-wrap bg-indigo-50/50 overflow-y-auto p-2 rounded-sm border-primary dark:bg-slate-700/50">
+        {cipherText}
+      </pre>
+    </div>
+  );
+}
---
+++ src/pages/encrypt/EncryptForm/index.tsx
@@ -0,0 +1,40 @@
+import { cx } from 'class-variance-authority';
+import { useState } from 'react';
+
+import { useAutoAnimate } from '@/lib/auto-animate';
+
+import { useCiphersPipeStore } from '../store';
+import { EncryptedResult } from './EncryptedResult';
+
+interface EncryptFormProps {
+  className?: string;
+}
+
+export function EncryptForm({ className }: EncryptFormProps) {
+  const [plainText, setPlainText] = useState<string>('');
+  const handlePlainTextChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLTextAreaElement>) => void setPlainText(value);
+
+  const isPipeEmpty = useCiphersPipeStore((s) => s.ciphers.length === 0);
+
+  const [contentRef] = useAutoAnimate<HTMLDivElement>();
+  return (
+    <div className={cx('flex flex-col', className)} ref={contentRef}>
+      <div className="w-full">
+        <label htmlFor="plaintext" className="sr-only">
+          Message to encrypt
+        </label>
+        <textarea
+          id="plaintext"
+          placeholder="Message to encrypt..."
+          rows={5}
+          className="block w-full rounded-sm resize-none border-primary text-sm focus-ring placeholder:font-light"
+          value={plainText || ''}
+          onChange={handlePlainTextChange}
+        />
+      </div>
+      {plainText && !isPipeEmpty && <EncryptedResult text={plainText} />}
+    </div>
+  );
+}
