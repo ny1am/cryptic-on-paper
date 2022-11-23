8047b3617a1a177a78becd28f7a4abab3484fe7d
Nazar Yavnyy <nazar.tools@pm.me>
2022-11-23 10:53:06 +0200

    style: make hero responsive to screen height

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,18 @@
+export function Hero() {
+  return (
+    <div className="pattern-bg flex flex-col justify-center min-h-[35vh] py-16 lg:py-24">
+      <div className="mx-auto max-w-7xl px-4 lg:px-6">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+            <span>Cryptic </span>
+            <span className="text-indigo-600 dark:text-indigo-300">on paper</span>
+          </h1>
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl dark:text-gray-400">
+            Encrypt messages with simple ciphers that you could replicate on a piece of
+            paper
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/encrypt/index.tsx
+++ src/pages/encrypt/index.tsx
@@ -1,3 +1,4 @@
+import { Hero } from '@/components/Hero';
 import { useAutoAnimate } from '@/lib/auto-animate';
 
 import { CiphersPipe } from './CiphersPipe';
@@ -10,21 +11,7 @@ export function EncryptPage() {
   const [contentRef] = useAutoAnimate<HTMLDivElement>();
   return (
     <>
-      <div className="pattern-bg py-16 lg:py-24">
-        <div className="mx-auto max-w-7xl px-4 lg:px-6">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
-              <span>Cryptic </span>
-              <span className="text-indigo-600 dark:text-indigo-300">on paper</span>
-            </h1>
-            <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl dark:text-gray-400">
-              Encrypt messages with simple ciphers that you could replicate on a piece of
-              paper
-            </p>
-          </div>
-        </div>
-      </div>
-
+      <Hero />
       <div
         ref={contentRef}
         className="mt-4 max-w-4xl mx-auto flex flex-wrap px-4 gap-4 lg:gap-10 lg:mt-10"
