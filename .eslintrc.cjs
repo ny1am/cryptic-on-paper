784a079f97a5044ea4d9404c92a7e97b46665b5c
Nazar Yavnyy <nazar.tools@pm.me>
2022-12-08 22:46:29 +0200

    chore: clean up configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,19 +36,10 @@ module.exports = {
   ignorePatterns: ['**/*.cjs'],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/accessible-emoji': 'off',
-    'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
