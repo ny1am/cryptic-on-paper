934f8526e4890765c3cb43a2d1a0b4a3d2f1357f
Nazar Yavnyy <nazar.tools@pm.me>
2022-12-31 17:08:30 +0200

    chore: e2e tests as a separate app

--- apps/web-e2e/cypress.config.ts
+++ apps/web-e2e/cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: 'src/support/e2e.ts',
+    specPattern: 'src/e2e/**/*.cy.ts',
   },
   video: false,
+  screenshotOnRunFailure: false,
 });
