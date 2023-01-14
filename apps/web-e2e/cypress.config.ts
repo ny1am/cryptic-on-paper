ba4c990dc6d488c604a866652cbf1d22062175ef
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-14 10:24:21 +0200

    chore(ci): run e2e on preview env

--- apps/web-e2e/cypress.config.ts
+++ apps/web-e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000/cryptic-on-paper',
     supportFile: 'src/support/e2e.ts',
     specPattern: 'src/e2e/**/*.cy.ts',
   },
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
+  preview: { port: 3000 },
   base: '/cryptic-on-paper/',
   plugins: [tsconfigPaths(), react()],
 });
