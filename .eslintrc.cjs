ce4c4d33b903045bcc939150c4f86e1d61d009b6
Nazar Yavnyy <nazar.tools@pm.me>
2023-02-12 11:26:03 +0200

    chore(eslint): deprecation plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,18 +7,20 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
+    project: './tsconfig.json',
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
-  plugins: ['simple-import-sort', 'prettier'],
+  plugins: ['deprecation', 'simple-import-sort', 'prettier'],
   ignorePatterns: ['**/*.cjs'],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'deprecation/deprecation': 'warn',
   },
 };
