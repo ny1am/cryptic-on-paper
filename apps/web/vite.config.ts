bb0e0b7aed1231c6f392c5bbdbdd4b6442e8cfd6
Nazar Yavnyy <nazar.tools@pm.me>
2023-02-25 12:09:23 +0200

    chore: support variables in index.html

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -7,5 +8,18 @@ export default defineConfig({
   server: { port: 3000 },
   preview: { port: 3000 },
   base: '/cryptic-on-paper/',
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Cryptic on paper',
+          description:
+            'Encrypt messages with simple ciphers that you could replicate on a piece of paper',
+        },
+      },
+    }),
+  ],
 });
