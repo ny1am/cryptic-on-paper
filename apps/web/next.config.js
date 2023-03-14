02bf7ad0c6469e87ce05d3dffdaec7127ce3debc
Nazar Yavnyy <nazar.tools@pm.me>
2023-03-14 08:21:22 +0200

    chore(nextjs): bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,14 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withAnalyzer({
   reactStrictMode: true,
   transpilePackages: ['@cop/*'],
   output: 'standalone',
-};
+});
 
 export default nextConfig;
